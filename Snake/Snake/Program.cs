2528cdd1a35c30e8b5b0c1e70825e12b54127db7
andrew-kulikov <andrew.1.kulikov@gmail.com>
2017-01-28 23:19:47 +0300

    List test

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,15 +8,22 @@ namespace Snake {
     class Program {
         static void Main(string[] args) {
             Point p1 = new Point(1, 1, '%');
-            /*p1.x = 1;
-            p1.y = 1;
-            p1.sym = '%';*/
             p1.Draw();
             Point p2 = new Point(7, 6, '*');
-            /*p2.x = 7;
-            p2.y = 6;
-            p2.sym = '*';*/
             p2.Draw();
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+            numList.RemoveAt(0);
+            foreach(int i in numList) {
+                Console.WriteLine(i);
+            }
+            Point p3 = new Point(2, 3, '-');
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
         }
     }
 }
