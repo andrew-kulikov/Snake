1315667e6b4250aec8f9892fc963fad2086291b0
andrew-kulikov <andrew.1.kulikov@gmail.com>
2017-01-29 18:20:31 +0300

    Inheritance principle

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 namespace Snake {
     class Program {
         static void Main(string[] args) {
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
+            HorizontalLine topLine = new HorizontalLine(0, 78, 0, '+');            
+            HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '+');            
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');           
+            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
+            topLine.Draw();
+            bottomLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
             Point p1 = new Point(1, 1, '%');
-            p1.Draw();
-            Point p2 = new Point(7, 6, '#');
-            p2.Draw();
-            HorizontalLine line1 = new HorizontalLine(0, 15, 0, '+');
-            line1.Draw();
-            HorizontalLine line2 = new HorizontalLine(0, 15, 8, '+');
-            line2.Draw();
+            p1.Draw();          
+            Console.ReadLine();
         }
     }
 }
--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -5,13 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Snake {
-    class HorizontalLine : Figure {
-        public HorizontalLine(int xLeft, int xRight, int y, char sym) {
+    class VerticalLine : Figure {
+        public VerticalLine(int x, int yUp, int yDown, char sym) {
             pList = new List<Point>();
-            for (int x = xLeft; x <= xRight; x++) {
+            for (int y = yUp; y <= yDown; y++) {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
-        }   
+        }
     }
 }
