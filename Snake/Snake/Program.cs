4040be74ac0b33edc9a4004ff2440a6f54015ab4
andrew-kulikov <andrew.1.kulikov@gmail.com>
2017-01-29 23:56:39 +0300

    key processing

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,6 +11,8 @@ namespace Snake {
             Console.SetWindowSize(1, 1);
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
+
+
             HorizontalLine topLine = new HorizontalLine(0, 78, 0, '+');            
             HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '+');            
             VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');           
@@ -19,16 +21,21 @@ namespace Snake {
             bottomLine.Draw();
             leftLine.Draw();
             rightLine.Draw();
+
+
             Point p = new Point(1, 1, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
+
+
+            while(true) {
+                if(Console.KeyAvailable) {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);                                                  
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
             Console.ReadLine();
         }
     }
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -31,5 +31,19 @@ namespace Snake {
             newPoint.Move(1, direction);
             return newPoint;
         }
+        public void HandleKey(ConsoleKey key) {
+            if (key == ConsoleKey.LeftArrow) {
+                direction = Direction.LEFT;
+            }
+            else if (key == ConsoleKey.RightArrow) {
+                direction = Direction.RIGHT;
+            }
+            else if (key == ConsoleKey.UpArrow) {
+                direction = Direction.UP;
+            }
+            else if (key == ConsoleKey.DownArrow) {
+                direction = Direction.DOWN;
+            }
+        }
     }
 }
